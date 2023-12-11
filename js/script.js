252cdef1aa2d07caf25abf0b123d4487e61fbb41
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-12-11 13:48:42 +0100

    added bonus image

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Ciao Fabio!'
+      message: 'Ciao piccolo hodler!',
+      image: `https://bitcoinmagazine.com/.image/t_share/MTk0OTEzOTYyMzU2NzEyNjEw/filc6b5xsaym1ge.jpg`
     }
   }
 }).mount('#app')
